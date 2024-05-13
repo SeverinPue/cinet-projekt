f37aca2dcb29ebb36267cba1c30b7ef6927ba5ef
SeverinPue <59147017+SeverinPue@users.noreply.github.com>
2024-05-13 16:07:55 +0200

    page.tsx profile

--- src/app/addAccount/page.tsx
+++ src/app/addAccount/page.tsx
@@ -1,10 +1,10 @@
-import { Form } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import { DatePicker } from "antd";
 
 export default function AddAccount() {
   return (
     <>
-      <img src="png/logoNoBackground.png" alt="logo_Sinet" />
+      <img src="png/logoNoBackground.png" width={"300px"} alt="logo_Sinet" />
 
       <p>Account erstellen</p>
       <p>Hast du schon ein Account? Hier anmelden.</p>
@@ -20,7 +20,9 @@ export default function AddAccount() {
         <Form.Control type="email" id="email" />
         <Form.Label htmlFor="datepicker">Geburtstag</Form.Label>
         <DatePicker id="datepicker"/>
-
+        <Button variant="primary" type="submit">
+        Submit
+      </Button>
       </Form>
     </>
   );

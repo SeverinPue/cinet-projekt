696387b1b7fb66fcb682b4d56d0b36989302b05d
SeverinPue <59147017+SeverinPue@users.noreply.github.com>
2024-05-13 15:18:52 +0200

    addaccount finished

--- src/app/addAccount/page.tsx
+++ src/app/addAccount/page.tsx
@@ -1,6 +1,5 @@
 import { Form } from "react-bootstrap";
-
-
+import { DatePicker } from "antd";
 
 export default function AddAccount() {
   return (
@@ -10,25 +9,17 @@ export default function AddAccount() {
       <p>Account erstellen</p>
       <p>Hast du schon ein Account? Hier anmelden.</p>
 
-
       <Form>
-
-      <Form.Label htmlFor="username">Username</Form.Label>
-      <Form.Control
-        type="text"
-        id="username"
-      />
-      <Form.Label htmlFor="inputPassword">Password</Form.Label>
-      <Form.Control
-        type="password"
-        id="inputPassword"
-      />
-      <Form.Label htmlFor="email">Email</Form.Label>
-      <Form.Control
-        type="email"
-        id="email"
-      />
-
+        <Form.Label htmlFor="username">Username</Form.Label>
+        <Form.Control type="text" id="username" />
+        <Form.Label htmlFor="inputPassword">Password</Form.Label>
+        <Form.Control type="password" id="inputPassword" />
+        <Form.Label htmlFor="email">Email</Form.Label>
+        <Form.Control type="email" id="email" />
+        <Form.Label htmlFor="email">Email</Form.Label>
+        <Form.Control type="email" id="email" />
+        <Form.Label htmlFor="datepicker">Geburtstag</Form.Label>
+        <DatePicker id="datepicker"/>
 
       </Form>
     </>

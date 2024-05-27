d402cc3ae4ddd40f18bdeaa39b1ba47bcb5efa42
AnonymUser1234 <anonym.junk@outlook.com>
2024-05-27 16:14:30 +0200

    lolli

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,41 @@
+'use client'
 import Link from "next/link";
 import BurgerMenu from "../burgermenu/page";
+import { createContext, use, useState } from "react";
+
+type PersonalDetails = {
+    name: String,
+    email: String,
+    password: String,
+    dateOfBirth: Date
+}
+
 
 export default function Profile(){
+    const [points, setPoints] = useState<number>(200);
+    const [personalDetails, setPersonalDetails] = useState<PersonalDetails>(
+        {name: "livio",
+        email: "livio.thommen@axa.ch",
+        password: "*******",
+        dateOfBirth: new Date('2008-01-27')});
+
+        function dateFormatter(date: Date){
+            return date.getDay+"."+date.getMonth+"."+date.getFullYear;
+        }
+
+
     return(
         <div>
-            <h1>Profil</h1>
             <BurgerMenu></BurgerMenu>
+            <h1>Profile</h1>
+            <div>
+                du hast {points} Punkte
+            </div>
+            <h2>Profilinformationen</h2>
+            <p>Name: {personalDetails.name}</p>
+            <p>Email: {personalDetails.email}</p>
+            <p>Passwort: {personalDetails.password}</p>
+            <p>Geburtsdatum: {personalDetails.dateOfBirth.toDateString()}</p>
         </div>
     )
 }
